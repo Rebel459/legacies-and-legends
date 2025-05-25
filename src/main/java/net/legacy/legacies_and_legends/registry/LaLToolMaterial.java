045e48ff5b5957beaa2186122ac4a167f0a5b45a
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2025-05-25 12:11:01 +1000

    1.21.4-v1.6.13

--- src/main/java/net/legacy/legacies_and_legends/registry/LaLToolMaterial.java
+++ src/main/java/net/legacy/legacies_and_legends/registry/LaLToolMaterial.java
@@ -22,8 +22,8 @@ import net.minecraft.world.level.block.Blocks;
 public record LaLToolMaterial(
         TagKey<Block> incorrectBlocksForDrops, int durability, float speed, float attackDamageBonus, int enchantmentValue, TagKey<Item> repairItems
 ) {
-    public static final ToolMaterial HOOK = new ToolMaterial(BlockTags.INCORRECT_FOR_IRON_TOOL, 250, 6.0F, 5.0F, 10, LaLItemTags.HOOK_REPAIR_MATERIALS);
-    public static final ToolMaterial KNIFE = new ToolMaterial(BlockTags.INCORRECT_FOR_DIAMOND_TOOL, 1561, 8.0F, 3.0F, 25, LaLItemTags.KNIFE_REPAIR_MATERIALS);
+    public static final ToolMaterial HOOK = new ToolMaterial(BlockTags.INCORRECT_FOR_IRON_TOOL, 250, 6.0F, 5.0F, 15, LaLItemTags.HOOK_REPAIR_MATERIALS);
+    public static final ToolMaterial KNIFE = new ToolMaterial(BlockTags.INCORRECT_FOR_DIAMOND_TOOL, 1561, 8.0F, 3.0F, 15, LaLItemTags.KNIFE_REPAIR_MATERIALS);
 
     public static final ToolMaterial VERDANT = new ToolMaterial(BlockTags.INCORRECT_FOR_STONE_TOOL, 184, 4.0F, 1.0F, 10,LaLItemTags.VERDANT_TOOL_MATERIALS);
     public static final ToolMaterial CLEAVING = new ToolMaterial(BlockTags.INCORRECT_FOR_IRON_TOOL, 292, 6.0F, 2.0F, 12, LaLItemTags.CLEAVING_TOOL_MATERIALS);
