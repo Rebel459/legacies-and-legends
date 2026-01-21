096f4dcdaafbbf93a2c69d7929fc2895e9b05c33
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-01-21 15:18:28 +1100

    v2.2.12

--- src/main/java/net/legacy/legacies_and_legends/tag/LaLBiomeTags.java
+++ src/main/java/net/legacy/legacies_and_legends/tag/LaLBiomeTags.java
@@ -7,9 +7,9 @@ import net.minecraft.world.level.biome.Biome;
 import org.jetbrains.annotations.NotNull;
 
 public class LaLBiomeTags {
-    public static final TagKey<Biome> MUSIC_SNOWY = bind("music_pool/has_snowy_music");
-    public static final TagKey<Biome> MUSIC_SAVANNA = bind("music_pool/has_savanna_music");
-    public static final TagKey<Biome> MUSIC_DARK_FOREST = bind("music_pool/has_dark_forest_music");
+    public static final TagKey<Biome> MUSIC_SNOWY = bind("music/snowy");
+    public static final TagKey<Biome> MUSIC_SAVANNA = bind("music/savanna");
+    public static final TagKey<Biome> MUSIC_DARK_FOREST = bind("music/dark_forest");
 
     @NotNull
     private static TagKey<Biome> bind(@NotNull String path) {
